39c69b54ecc2789ca3604e85c78a922396854c40
Kevin Pastrana <u20232215370@usco.edu.co>
2024-02-19 17:52:17 -0500

    Ejercicios del PDF

---
+++ src/Ejercicios_PDF/Ejercicio_primero.java
@@ -0,0 +1,42 @@
+package Ejercicios_PDF;
+
+import java.util.Scanner;
+
+/*@author Kevin Felipe*/
+ /*En una tienda de computadoras se planea ofrecer a los clientes un descuento que dependerá 
+del número de equipos que compre. Si las computadoras son menos de cinco se les dará un 
+10% de descuento sobre el total de la compra; si el número de computadoras es mayor o igual 
+a cinco pero menos de diez se le otorga un 20% de descuento; y si son 10 o más se les da un 
+40% de descuento. El precio de cada computadora es de U$500. */
+public class Ejercicio_primero {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // Crea un objeto Scanner
+        System.out.println("¿Cuantas computadoras quieres llevar?");
+
+        int computadoras = sc.nextInt();  // Lee la entrada del usuario como un entero
+        sc.nextLine();  // Limpia el buffer de entrada
+
+        System.out.println("Las computadoras que llevas son: " + computadoras);  // Salida de la entrada del usuario
+
+        int precio = 0;
+        int descuento = 0;
+        int total = 0;
+
+        if (computadoras <= 5) { // Verifica si son menos de 5 computadoras para aplicar un 10% de descuento
+            precio = computadoras * 500;
+            descuento = (int) (precio * 0.10);
+            total = precio - descuento;
+        } else if (computadoras >= 5 && computadoras < 10) { // Verifica si el número de computadoras es mayor o igual a 5 y menor que 10 para aplicar un 20% de descuento
+            precio = computadoras * 500;
+            descuento = (int) (precio * 0.20);
+            total = precio - descuento;
+        } else if (computadoras >= 10) { // Verifica si son mas de 10 computadoras para aplicar un 40% de descuento
+            precio = computadoras * 500;
+            descuento = (int) (precio * 0.40);
+            total = precio - descuento;
+        }
+        System.out.println("El precio total de las computadoras es de: " + precio);
+        System.out.println("El precio total con descuento es de: " + total);
+    }
+}
---
+++ src/Ejercicios_PDF/Ejercicio_segundo.java
@@ -0,0 +1,38 @@
+package Ejercicios_PDF;
+
+import java.util.Scanner;
+
+/*@author Kevin Felipe*/
+ /*En una serviteca se ha establecido una promoción de las llantas marca “Pinchadas”, que consiste 
+en lo siguiente: 
+• Si se compran menos de cinco llantas el precio es de U$100 cada una, de U$75 si se 
+compran de cinco a 10 y de U$50 si se compran más de 10. 
+• Obtener la cantidad de dinero que una persona tiene que pagar por cada una de las llantas 
+que compra y la que tiene que pagar por el total de la compra.*/
+public class Ejercicio_segundo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Crea un objeto Scanner
+        System.out.println("¿Cuantas llantas desea comprar?");
+
+        int llantas = sc.nextInt(); // Lee la entrada del usuario como un entero
+        sc.nextLine(); // Limpia el buffer de entrada
+
+        System.out.println("Las llantas que llevas son: " + llantas); // Salida de la entrada del usuario
+
+        int precio = 0;
+
+        if (llantas <= 5) {
+            precio = 100;
+        } else if (llantas >= 5 && llantas <= 10) {
+            precio = 75;
+        } else if (llantas > 10) {
+            precio = 50;
+        }
+
+        int precioTotal = llantas * precio;
+
+        System.out.println("El precio por cada llanta es: $" + precio);
+        System.out.println("La cantidad total a pagar por las llantas es: $" + precioTotal);
+    }
+}
